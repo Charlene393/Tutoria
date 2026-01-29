270ed3373c3aeea5f7249b411529546cda467a27
Charlene393 <charlenembugua0@gmail.com>
2026-01-29 08:51:12 +0300

    Retoration of the app foldert

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function _layout() {
+  return (
+    <View>
+      <Text>_layout</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ app/_layout.tsx
@@ -0,0 +1,36 @@
+
+import { ThemeProvider } from "@/context/theme.context";
+import {
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+    useFonts,
+} from "@expo-google-fonts/poppins";
+import { Stack } from "expo-router";
+import React from "react";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+// SplashScreen.preventAutoHideAsync();
+
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    Poppins_600SemiBold,
+    Poppins_300Light,
+    Poppins_700Bold,
+    Poppins_400Regular,
+    Poppins_500Medium,
+  });
+
+  return (
+    <ThemeProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="(routes)/onboarding/index" />
+        </Stack>
+    </ThemeProvider>
+  );
+}
